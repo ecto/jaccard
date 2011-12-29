56f22f441c1fa9f2f9677a36bb3903bedb305264
Cam Pedersen <diffference@gmail.com>
2011-12-29 16:30:07 -0500

    implement shit

---
+++ jaccard.js
@@ -0,0 +1,51 @@
+/*
+ * jaccard
+ * <cam@onswipe.com>
+ */
+
+/*
+ * Return a count of mutual elements in the input sets
+ */
+var intersection = function () {
+  var x = [];
+  for (var i in arguments) {
+    
+  }
+}
+
+/*
+ * Return a count of all distinct elements from both input sets
+ */
+var union = function () {
+  var x = [];
+  for (var i in arguments) {
+    for (var j in arguments[i]) {
+      if (~x.indexOf(arguments[i][j])) {
+        x.push(a[i]);
+      }
+    }
+  }
+  return x.length;
+}
+
+/*
+ * Similarity
+ */
+var index = function (a, b) {
+  return intersection(a, b) / union(a, b);
+}
+
+/*
+ * Dissimilarity
+ */
+var distance = function (a, b) {
+  return 1 - index(a, b);
+}
+
+/*
+ * Say cheese
+ */
+module.exports = {
+  index: index,
+  distance: distance
+}
