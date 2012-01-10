3c945f568ae7341e19b489a62f467ed92a040ed8
Cam Pedersen <diffference@gmail.com>
2012-01-10 16:49:52 -0500

    [major] add async

--- jaccard.js
+++ jaccard.js
@@ -3,33 +3,52 @@
  * <cam@onswipe.com>
  */
 
+var async = require('async');
+
 /*
  * Return mutual elements in the input sets
  */
 var intersection = function (a, b, c) {
   var x = [];
-
-  a.forEach(function (e) {
+  var check = function (e, cb) {
     if (e in b) x.push(e);
-  });
+    if (cb && typeof cb == 'function') cb(null);
+  };
 
-  return x;
+  if (c) {
+    async.forEach(a, check, function () {
+      c(null, x);
+    });
+  } else {
+    a.forEach(check);
+    return x;
+  }
 }
 
 /*
  * Return distinct elements from both input sets
  */
-var union = function (a, b) {
+var union = function (a, b, c) {
   var x = [];
-
-  var check = function (e) {
+  var check = function (e, cb) {
     if (!~x.indexOf(e)) x.push(e);
+    if (cb && typeof cb == 'function') cb(null);
   }
 
-  a.forEach(check);
-  b.forEach(check);
-
-  return x;
+  if (c) {
+    var waiting = 2;
+    var asyncCheck = function () {
+      if (--waiting == 0) {
+        c(null, x);
+      }
+    }
+    async.forEach(a, check, asyncCheck);
+    async.forEach(b, check, asyncCheck);
+  } else {
+    a.forEach(check);
+    b.forEach(check);
+    return x;
+  }
 }
 
 /*
@@ -37,7 +56,16 @@ var union = function (a, b) {
  */
 var index = function (a, b, c) {
   if (c) {
-    c(intersection(a, b).length / union(a, b).length);
+    async.parallel({
+      intersection: function (cb) {
+        intersection(a, b, cb);
+      },
+      union: function (cb) {
+        union(a, b, cb);
+      }
+    }, function (err, results) {
+      c(results.intersection.length / results.union.length);
+    });
   } else {
     return intersection(a, b).length / union(a, b).length;
   }
